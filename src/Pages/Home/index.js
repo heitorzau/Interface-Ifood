d76141a25c397d19032998e346cb9f024b4453aa
Heitor Barbosa <73502938+heitorzau@users.noreply.github.com>
2022-03-09 11:40:01 -0300

    Add files via upload
    Projeto concluído.

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView, Image, View, ButtonContainer } from './style.js';
+
+import banner from '../../assets/banner.png';
+import Button from '../../components/Button';
+
+export default function Home({ navigation }) {
+  return (
+    <>
+      <StatusBar style="theme-dark" />
+      <SafeAreaView>
+        <View>
+          <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Pedir comida nunca foi tão fácil</Text>
+          <Image source={banner} />
+          <Text style={{ fontWeight: 'bold', fontSize: 16 }}>Permitir Localização</Text>
+          <Text>Para descobrir restaurantes que entregam em sua região</Text>
+        </View>
+        <ButtonContainer>
+          <Button text="Pular" onPress={() => navigation.navigate('Main')} />
+          <Button text="Entrar" theme="primary" />
+        </ButtonContainer>
+      </SafeAreaView>
+    </>
+  );
+}
